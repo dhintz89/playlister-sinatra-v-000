7c906a6f465b50be9e8365c6363457cf8e4ee790
Dan Hintz <dhintz@live.com>
2019-05-19 22:33:09 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,6 +36,7 @@ class SongsController < ApplicationController
   
   patch "/songs/:slug" do 
     @song = Song.find_by_slug(params[:slug])
+    @song.name = params[:song_name]
     @song.artist = Artist.find_or_create_by(name: params[:artist_name])
     if !@song.genres.empty?
       @song.genres = []
